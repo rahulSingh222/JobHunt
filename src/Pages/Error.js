270f35fc7c23f14a3520484ee9e7438aeb70126d
rahulsingh222 <rsingh.rahul224@gmail.com>
2023-04-22 19:03:44 +0530

    Error page completed

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import errorImg from "../assets/images/not-found.svg"
+import Wrapper from "../assets/wrappers/ErrorPage"
 
 function Error() {
   return (
-    <div>Error</div>
+      <Wrapper className="full-page">
+        <div>
+          <img src={errorImg} alt="not found" />
+          <h3>Ohh! something wrong in the heaven</h3>
+          <p>We are unable to find page you are looking for, kindly refresh or try another.</p>
+          <Link to="/">back to Home</Link>
+        </div>
+      </Wrapper>
   )
 }
 
