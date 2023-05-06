93838d92a0c495aadf48a83b2b26e28df77ec7ed
rahulsingh222 <rsingh.rahul224@gmail.com>
2023-05-06 20:00:59 +0530

    Profile page completed successfully.

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -48,6 +48,10 @@ export const updateUser = createAsyncThunk(
       });
       return resp.data;
     } catch (error) {
+      if(error.response.status === 401){
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue('Unauthorized! Logging out...')
+      }
       console.log(error.response);
       return thunkAPI.rejectWithValue(error.response.data.msg);
     }
